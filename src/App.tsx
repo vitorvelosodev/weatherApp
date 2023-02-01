27ad7000d44ce643b58f8b8d38dd8ea8758fe54b
Vitor Veloso <vitor.asv@gmail.com>
2023-01-31 22:50:28 -0300

    feat(api): aPI call working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from 'react'
 import { fetchApiData } from './scripts/api-request'
-import data from './scripts/data.json'
+import dataStd from './scripts/data.json'
 import weatherCodes from './scripts/weatherCodes.json'
 import MainWeatherScreen from './components/MainWeatherScreen/MainWeatherScreen'
 
 function App() {
-  const allWeatherData = () => {
+  const [ data , setData ] = useState(dataStd)
+  // console.log(data)
+
+  const [ render, setRender ] = useState(false)
+
+  const allWeatherData = () => {    
     const location = data.location.name
     const temperature = data.current.temperature
     const weatherCode = data.current.weather_code
@@ -25,26 +30,26 @@ function App() {
     )
   }
 
-  // useEffect(() => {
-  //   // console.log(data)
-  //   const apiKey = import.meta.env.VITE_API_KEY
-  //   console.log('APIKEY', apiKey)
-  //   // 6fc24338b3868a98d96f1acd2893285a
-  //   // const URL = `http://api.weatherstack.com/current?access_key=${apiKey}&query=New York`
-  //   // console.log(URL)
-  //   const call = async () => {
-  //     const response = await fetchApiData(URL)
-  //     console.log(response)
-  //     return response
-  //   }
-  //   const response = call()
-  // }, [])
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_API_KEY
+    // const URL = `http://api.weatherstack.com/current?access_key=${apiKey}&query=Goiania`
+    const call = async () => {
+      // const response = await fetchApiData(URL)
+      // console.log(response)
+      // setData(response.data)
+      setData(dataStd)
+      setRender(true)
+    }
+    call()
+  }, [])
 
   return (
     <>
-      <MainWeatherScreen
-        {...allWeatherData()}
-      />
+      { render &&
+        <MainWeatherScreen
+          {...allWeatherData()}
+        />
+      }
     </>
   )
 }
